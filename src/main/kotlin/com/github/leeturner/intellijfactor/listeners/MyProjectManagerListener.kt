af6b507d4370332348ebfa8dc8a577bf5f019ed1
GitHub Action <action@github.com>
2023-01-21 16:40:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/leeturner/intellijfactor/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/leeturner/intellijfactor/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.leeturner.intellijfactor.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.leeturner.intellijfactor.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/leeturner/intellijfactor/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/leeturner/intellijfactor/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.leeturner.intellijfactor.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leeturner.intellijfactor.MyBundle
 
 class MyApplicationService {
 
