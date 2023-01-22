cbab9c9b0591cafac8defb448c3c7a57576081db
leeturner <lee@leeturner.me>
2023-01-22 18:06:50 +0000

    specify the default charset

--- src/main/kotlin/com/leeturner/intellijfactor/language/FactorFileType.kt
+++ src/main/kotlin/com/leeturner/intellijfactor/language/FactorFileType.kt
@@ -1,10 +1,12 @@
 package com.leeturner.intellijfactor.language
 
 import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.vfs.VirtualFile
 
 object FactorFileType : LanguageFileType(FactorLanguage) {
     override fun getName() = "Factor"
     override fun getDescription() = "Factor language file"
     override fun getDefaultExtension() = "factor"
     override fun getIcon() = FactorIcons.fileTypeIcon
+    override fun getCharset(file: VirtualFile, content: ByteArray): String = "UTF-8"
 }
