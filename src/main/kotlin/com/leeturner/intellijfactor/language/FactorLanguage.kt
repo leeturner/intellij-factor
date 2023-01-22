d8463e2c81174a9d67630f474c8c2bfa96c7827e
leeturner <lee@leeturner.me>
2023-01-22 18:06:22 +0000

    Add additional mime types

--- src/main/kotlin/com/leeturner/intellijfactor/language/FactorLanguage.kt
+++ src/main/kotlin/com/leeturner/intellijfactor/language/FactorLanguage.kt
@@ -2,7 +2,7 @@ package com.leeturner.intellijfactor.language
 
 import com.intellij.lang.Language
 
-object FactorLanguage : Language("Factor", "text/factor") {
+object FactorLanguage : Language("Factor", "Factor", "text/x-factor", "application/x-factor", "text/factor") {
     override fun isCaseSensitive() = true
     override fun getDisplayName() = "Factor"
 }
